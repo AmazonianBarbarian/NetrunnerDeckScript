9c6f971e6857d545d4d223724ba13d76a8f91dc6
Astrid La Reine Lockhart <astrid.lr.lockhart@gmail.com>
2025-07-23 15:24:22 -0500

    Update NetRunnerDeckMain.py

--- NetRunnerDeckMain.py
+++ NetRunnerDeckMain.py
@@ -1,8 +1,7 @@
-# [] Display cards in a specific resolution.
-# [] Display Deck and currently activated cards.
-#
-#
-#
+# Uses the PNG files from the digital version of the Netrunner Deck for Cyberpunk RED.
+# [ https://www.drivethrurpg.com/en/product/372730/netrunning-deck ] You can by the files here.
+# Output opens the Cards using the default image viewer.
+
 from PIL import Image
 
 #using a with statement so I don't have to close the file manually.
